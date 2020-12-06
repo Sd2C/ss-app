2d00fef2a9802a596657d3bcf84177b7e6767b90
Sd2C <semaouiseddik@gmail.com>
2020-12-06 12:42:23 +0100

    new

--- js/script.js
+++ js/script.js
@@ -1,12 +1,48 @@
-var css=document.querySelector("h3");
-var c1=document.querySelector(".c1");
-var c2=document.querySelector(".c2");
-var body=document.getElementById("gradient");
+function myFunction() {
+var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
+$(document).ready(function(){
+  $('.nav-button').click(function(){
+       $('.nav-button').toggleClass('change');
+      
+  });
+$(window).scroll(function(){
+   let pos=$(this).scrollTop();
+   if(pos >= 200){
+       $('.nav-menu').addClass('custom-navbar');
+   }else $('.nav-menu').removeClass('custom-navbar');
 
-function setGradient(){
-	body.style.background="linear-gradient(to right, "+c1.value+","+c2.value+")";
-	css.textContent=body.style.background+";";
-}
-c1.addEventListener("input", setGradient);
-c2.addEventListener("input", setGradient);
+});
 
+$(window).scroll(function(){
+   let kos=$(this).scrollTop();
+  console.log(kos);
+  if(kos >= 650){
+      $('.camera-img').addClass('fromLeft');
+      $('.mission-text').addClass('fromRight');
+  }else{
+      $('.camera-img').removeClass('fromLeft');
+      // $('.camera-img').addClass('fromLeftGo');
+      $('.mission-text').removeClass('fromRight');
+  }
+  }) 
+ $('.gallery-list-item').click(function(){
+     let val=$(this).attr('data-filter');
+     let vall=$(this).attr('class');
+  //    console.log(vall);
+     if(val==='all'){
+         $('.filter').show(300 );
+     }else{
+      $('.filter').not('.'+val).hide(300);
+      $('.filter').filter('.'+val).show(300); 
+      }
+  });
+  $('.gallery-list-item').click(function(){
+      $(this).addClass('active-item').siblings().removeClass('active-item');
+ }); 
+});
